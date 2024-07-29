53cc97a6890f02c2ef32348e0a57f2b63fbb9257
chenyinjie <540589641@qq.com>
2024-07-29 10:23:32 +0800

    2024/7/28 server code update

--- src/server.cpp
+++ src/server.cpp
@@ -9,49 +9,69 @@
 #define SIZE 1024
 
 int main() {
+    std::cout << "\n------------Sever start------------\n";
     int server_socket_fd, new_socket_fd;
-    struct sockaddr_in server_socket_info;
+    struct sockaddr_in server_socket_addr;
     char buffer[SIZE];
     int opt = 1;
-    socklen_t server_socket_struct_len = sizeof(server_socket_info);
+    socklen_t server_sock_len = sizeof(server_socket_addr);
+
     // create server welcome socket file descriptor
     if ((server_socket_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-        perror("create server welcome socket failed.");
+        perror("create server's welcome socket failed.");
         exit(EXIT_FAILURE);
     }
+    std::cout << "create server's welcome socket success." << std::endl;
+
     // set server welcome socket's opt
     if (setsockopt(server_socket_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) < 0) {
         perror("set server welcome socket's opt failed.");
         exit(EXIT_FAILURE);
     }
-    // set welcome socket, bind it to fixed port
-    server_socket_info.sin_family = AF_INET;
-    server_socket_info.sin_port = htons(PORT);
-    server_socket_info.sin_addr.s_addr = INADDR_ANY;
+    std::cout << "set server's welcome socket opt success." << std::endl;
 
-    if (bind(server_socket_fd, (struct sockaddr*) &server_socket_info, server_socket_struct_len) < 0) {
+    // set welcome socket, bind it to fixed port
+    server_socket_addr.sin_family = AF_INET;
+    server_socket_addr.sin_port = htons(PORT);
+    server_socket_addr.sin_addr.s_addr = INADDR_ANY;
+    std::cout << "set welcome socket info success." << std::endl;
+
+    if (bind(server_socket_fd, (struct sockaddr*) &server_socket_addr, server_sock_len) < 0) {
         perror("bind server sock info to server_sock_struct failed.");
         exit(EXIT_FAILURE);
     }
-    // create new socket to accept new connection request
+    std::cout << "bind server's welcome socket to fixed port success." << std::endl;
+
+    std::cout << "start listening" << std::endl;
+    // listen to connection
     if (listen(server_socket_fd, 3) < 0) {
         perror("listen failed.");
         exit(EXIT_FAILURE);
     }
-    if ((new_socket_fd = accept(server_socket_fd, (struct sockaddr*) &server_socket_info, &server_socket_struct_len)) < 0) {
-        perror("accept failed.");
-        exit(EXIT_FAILURE);
-    }
-    // read message
-    memset(buffer, 0, SIZE);
-    if (read(new_socket_fd, buffer, SIZE) < 0) {
-        perror("read failed.");
-    } else {
-        std::cout << "Message from client: " << buffer << std::endl;
-    }
 
-    // close connection
-    close(new_socket_fd);
+    std::cout << "Start accept:" << std::endl;
+    while (true) {
+        if ((new_socket_fd = accept(server_socket_fd, (struct sockaddr*)& server_socket_addr, &server_sock_len)) < 0) {
+            perror("Accept failed.\n");
+            exit(EXIT_FAILURE);
+        }
+        std::cout << "Connection accepted.\n";
+        while (true) {
+            memset(buffer, 0, SIZE);
+            int valread = read(new_socket_fd, buffer, SIZE - 1);
+            if (valread <= 0) {
+                std::cerr << "Client disconnected or read error" << std::endl;
+                break;
+            } else {
+                buffer[valread] = '\0';
+                std::cout << "Message from client: " << buffer << std::endl;
+            }
+            std::string response = "Message recieved.    --server";
+            send(new_socket_fd, response.c_str(), response.size(), 0);
+        }
+        close(new_socket_fd);
+        std::cout << "Connection close." << std::endl;
+    }
     close(server_socket_fd);
 
     return 0;
