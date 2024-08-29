e7eb66b2a7c36cdc7a7e564bf8857ae3c0f0d541
chenyinjie <540589641@qq.com>
2024-08-29 11:20:14 +0800

    2024/8/29 update block_queue.h and add test_blockqueue.cpp

--- include/block_queue.h
+++ include/block_queue.h
@@ -13,7 +13,7 @@
 template <typename T>
 class BlockQueue {
 private:
-    std::mutex m_queue_mutex;
+    mutable std::mutex m_queue_mutex;
     std::condition_variable m_queue_condition_var;
 
     std::vector<T> m_queue_array;
@@ -24,33 +24,35 @@ private:
 
 public:
     explicit BlockQueue(int max_size = 1000);
+    BlockQueue(const BlockQueue&) = delete;
+    BlockQueue& operator=(const BlockQueue&) = delete;
     ~BlockQueue();
 
     void clear();
-
     bool isFull() const;
     bool isEmpty() const;
+    int get_size() const;
+    int get_max_size() const;
 
     bool get_first_element(T& value) const;
     bool get_last_element(T& value) const;
 
-    int get_size() const;
-    int get_max_size() const;
-
     bool push(const T& element);
-
     bool pop(T& element);
     bool pop(T& element, int ms_timeout);
 };
 
 template <typename T>
-BlockQueue<T>::BlockQueue(int max_size) {
-    if (max_size <= 0) exit(-1);
-    m_queue_max_size = max_size;
-    m_queue_array.resize(max_size);
-    m_queue_size = 0;
-    m_queue_first = -1;
-    m_queue_last = -1;
+BlockQueue<T>::BlockQueue(int max_size)
+    : m_queue_max_size(max_size),
+      m_queue_array(max_size),
+      m_queue_size(0),
+      m_queue_first(0),
+      m_queue_last(-1) {
+    if (max_size <= 0) {
+        std::cerr << "Invalid max size" << std::endl;
+        exit(-1);
+    }
 }
 
 template <typename T>
@@ -60,7 +62,7 @@ template <typename T>
 void BlockQueue<T>::clear() {
     std::lock_guard<std::mutex> lock(m_queue_mutex);
     m_queue_size = 0;
-    m_queue_first = -1;
+    m_queue_first = 0;
     m_queue_last = -1;
 }
 
@@ -74,6 +76,18 @@ template <typename T>
 bool BlockQueue<T>::isEmpty() const {
     std::lock_guard<std::mutex> lock(m_queue_mutex);
     return m_queue_size == 0;
+}
+
+template <typename T>
+int BlockQueue<T>::get_size() const {
+    std::lock_guard<std::mutex> lock(m_queue_mutex);
+    return m_queue_size;
+}
+
+template <typename T>
+int BlockQueue<T>::get_max_size() const {
+    std::lock_guard<std::mutex> lock(m_queue_mutex);
+    return m_queue_max_size;
 }
 
 template <typename T>
@@ -93,25 +107,12 @@ bool BlockQueue<T>::get_last_element(T& value) const {
 }
 
 template <typename T>
-int BlockQueue<T>::get_size() const {
-    std::lock_guard<std::mutex> lock(m_queue_mutex);
-    return m_queue_size;
-}
-
-template <typename T>
-int BlockQueue<T>::get_max_size() const {
-    std::lock_guard<std::mutex> lock(m_queue_mutex);
-    return m_queue_max_size;
-}
-
-template <typename T>
 bool BlockQueue<T>::push(const T& element) {
     std::lock_guard<std::mutex> lock(m_queue_mutex);
     if (m_queue_size >= m_queue_max_size) {
         m_queue_condition_var.notify_all();
         return false;
     }
-
     m_queue_last = (m_queue_last + 1) % m_queue_max_size;
     m_queue_array[m_queue_last] = element;
     ++m_queue_size;
