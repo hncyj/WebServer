ed544a3746ffc33457289454c0753372b362ad7c
chenyinjie <540589641@qq.com>
2024-09-10 11:03:17 +0800

    2024/09/10 update buffer.h and buffer.cpp.

--- src/buffer/buffer.cpp
+++ src/buffer/buffer.cpp
@@ -11,122 +11,106 @@
 
 Buffer::Buffer(int init_size): buffer_(init_size), readPos_(0), writePos_(0) {}
 
-size_t Buffer::readableLen() const {
-    std::lock_guard<std::mutex> lock(buffer_mtx_);
-    return writePos_ - readPos_;
+size_t Buffer::ReadableLen() const {
+  return writePos_ - readPos_;
 }
 
-size_t Buffer::writableLen() const {
-    std::lock_guard<std::mutex> lock(buffer_mtx_);
+size_t Buffer::WritableLen() const {
     return buffer_.size() - writePos_;
 }
 
-const char* Buffer::readPtr() const {
-    std::lock_guard<std::mutex> lock(buffer_mtx_);
-    return bufferPtr() + readPos_;
+const char* Buffer::ReadPtr() const {
+    return BufferPtr() + readPos_;
 }
 
-const char* Buffer::writePtr() const {
-    std::lock_guard<std::mutex> lock(buffer_mtx_);
-    return bufferPtr() + writePos_;
+const char* Buffer::WritePtr() const {
+    return BufferPtr() + writePos_;
 }
 
-void Buffer::retrive(size_t len) {
-    std::lock_guard<std::mutex> lock(buffer_mtx_);
-    if (len <= readableLen()) {
-        readPos_ += len;
-    } else {
-        retriveAll();
-    }
+std::string Buffer::ReadAllToStr() {
+  std::lock_guard<std::mutex> lock(buffer_mtx_);
+  size_t readable_len = ReadableLen();
+  std::string str(ReadPtr(), readable_len);
+  readPos_ += readable_len;
+
+  return str;
 }
 
-void Buffer::retriveAll() {
+void Buffer::Append(const char* data, size_t len) {
     std::lock_guard<std::mutex> lock(buffer_mtx_);
-    readPos_ = 0;
-    writePos_ = 0;
-}
-
-std::string Buffer::retriveAll2Str() {
-    std::lock_guard<std::mutex> lock(buffer_mtx_);
-    std::string str(readPtr(), readableLen());
-    retriveAll();
-    return str;
-}
-
-void Buffer::append(const std::string& str) {
-    std::lock_guard<std::mutex> lock(buffer_mtx_);
-    assert(!str.empty());
-    append(str.data(), str.size());
-}
-
-void Buffer::append(const char* data, size_t len) {
-    std::lock_guard<std::mutex> lock(buffer_mtx_);
-    ensureWritable(len);
-    std::copy(data, data + len, writePtr());
+    EnsureWritable(len);
+    std::copy(data, data + len, WritePtr());
     writePos_ += len;
 }
 
-void Buffer::append(const Buffer& buf) {
-    std::lock_guard<std::mutex> lock(buffer_mtx_);
-    append(buf.readPtr(), buf.readableLen());
+void Buffer::Append(const std::string& str) {
+    Append(str.data(), str.size());
 }
 
-ssize_t Buffer::readFd(int fd, int* saveErrno) {
+void Buffer::Append(const Buffer& buf) {
+    Append(buf.ReadPtr(), buf.ReadableLen());
+}
+
+ssize_t Buffer::ReadFromFd(int fd, int* saveErrno) {
     std::lock_guard<std::mutex> lock(buffer_mtx_);
     char buff[65535];
     struct iovec iov[2];
-    const size_t write_size = writableLen();
-    iov[0].iov_base = bufferPtr() + writePos_;
+    size_t write_size = WritableLen();
+    iov[0].iov_base = BufferPtr() + writePos_;
     iov[0].iov_len = write_size;
     iov[1].iov_base = buff;
     iov[1].iov_len = sizeof(buff);
 
-    const ssize_t len = readv(fd, iov, 2);
+    ssize_t len = readv(fd, iov, 2);
     if (len < 0) {
         *saveErrno = errno;
     } else if (static_cast<size_t>(len) <= write_size) {
         writePos_ += len;
     } else {
         writePos_ = buffer_.size();
-        append(buff, len - write_size);
+        size_t extra_len = len - write_size;
+        EnsureWritable(extra_len);
+        std::copy(buff, buff + extra_len, BufferPtr() + writePos_);
+        writePos_ += extra_len;
     }
+
     return len;
 }
 
-ssize_t Buffer::writeFd(int fd, int* saveErrno) {
+ssize_t Buffer::WriteToFd(int fd, int* saveErrno) {
     std::lock_guard<std::mutex> lock(buffer_mtx_);
-    size_t read_size = readableLen();
-    ssize_t len = write(fd, readPtr(), read_size);
+    size_t read_size = ReadableLen();
+
+    ssize_t len = write(fd, ReadPtr(), read_size);
     if (len < 0) {
         *saveErrno = errno;
         return len;
     }
     readPos_ += len;
+
     return len;
 }
 
-char* Buffer::bufferPtr() {
+char* Buffer::BufferPtr() {
     return buffer_.data();
 }
 
-const char* Buffer::bufferPtr() const {
+const char* Buffer::BufferPtr() const {
     return buffer_.data();
 }
 
-void Buffer::expandBuffer(size_t len) {
-    std::lock_guard<std::mutex> lock(buffer_mtx_);
-    if (writableLen() + readPos_ < len) {
+void Buffer::ExpandBuffer(size_t len) {
+    if (WritableLen() + readPos_ < len) {
         buffer_.resize(writePos_ + len + 1);
     } else {
-        std::copy(bufferPtr() + readPos_, bufferPtr() + writePos_, bufferPtr());
+        std::copy(BufferPtr() + readPos_, BufferPtr() + writePos_, BufferPtr());
         readPos_ = 0;
-        writePos_ = readableLen();
+        writePos_ = ReadableLen();
     }
 }
 
-void Buffer::ensureWritable(size_t len) {
-    std::lock_guard<std::mutex> lock(buffer_mtx_);
-    if (writableLen() < len) {
-        expandBuffer(len);
+void Buffer::EnsureWritable(size_t len) {
+    if (WritableLen() < len) {
+        ExpandBuffer(len);
     }
 }
--- src/buffer/buffer.h
+++ src/buffer/buffer.h
@@ -21,33 +21,30 @@ public:
     explicit Buffer(int init_size = 1024);
     ~Buffer() = default;
 
-    size_t readableLen() const;
-    size_t writableLen() const;
+    size_t ReadableLen() const;                 // 当前可读数据长度
+    size_t WritableLen() const;                 // 当前可写数据长度
+    const char* ReadPtr() const;                // 可读数据起始指针
+    const char* WritePtr() const;               // 可写数据起始指针
     
-    const char* readPtr() const;
-    const char* writePtr() const;
+    std::string ReadAllToStr();                 // 读取缓存区全部可读数据转存为字符串并移动读指针
     
-    void retrive(size_t);
-    void retriveAll();
-    std::string retriveAll2Str();
-    
-    void append(const std::string&);
-    void append(const char*, size_t);
-    void append(const Buffer&);
+    void Append(const char*, size_t);           // 追加指定长度数据至缓冲区写指针后
+    void Append(const std::string&);            // 追加字符串数据至缓冲写指针后
+    void Append(const Buffer&);                 // 追加另一缓冲区数据至缓冲区写指针后
 
-    ssize_t readFd(int, int*);
-    ssize_t writeFd(int, int*);
+    ssize_t ReadFromFd(int, int*);              // 从指定文件描述符中读取数据到缓存区中
+    ssize_t WriteToFd(int, int*);               // 将缓存区中的数据写入文件描述符
 
 private:
-    std::vector<char> buffer_;
-    std::atomic<size_t> readPos_;
-    std::atomic<size_t> writePos_;
-    mutable std::mutex buffer_mtx_;
+    std::vector<char> buffer_;                  // 缓存区字符数组
+    size_t readPos_;                            // 缓存区读取位置
+    size_t writePos_;                           // 缓存区写入位置
+    mutable std::mutex buffer_mtx_;             // 缓存区互斥锁
     
-    char* bufferPtr();
-    const char* bufferPtr() const;
-    void expandBuffer(size_t);
-    void ensureWritable(size_t);
+    char* BufferPtr();                          // 缓存区指针
+    const char* BufferPtr() const;              // 缓存区指针常量
+    void ExpandBuffer(size_t);                  // 缓存区扩容
+    void EnsureWritable(size_t);                // 缓存区可写长度判断
 };
 
 #endif
