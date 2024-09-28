2621a3c92f1356418df82130fcff26f4cbd6351c
chenyinjie <540589641@qq.com>
2024-09-28 10:48:33 +0800

    2024/09/28 revised configuration.cpp

--- src/config/configuration.cpp
+++ src/config/configuration.cpp
@@ -6,46 +6,59 @@
 
 #include "configuration.h"
 
-#include <unistd.h>
 #include <cstdlib>
+#include <cstring>
+#include <iostream>
 
-Configuration::Configuration(): PORT(8080), ASYNC_LOG_MODE(0), SQL_CONNECT_NUMS(8), THREAD_NUMS(8) {};
+Configuration::Configuration() : PORT(8080), ASYNC_LOG_MODE(0), SQL_CONNECT_NUMS(8), THREAD_NUMS(8) {}
 
 void Configuration::parse_args(int argc, char* argv[]) {
-    int opt;
-    const char *str = "p:l:c:t:h";
-    while ((opt = getopt(argc, argv, str)) != -1) {
-        switch (opt) {
-            case 'p': {
-                PORT = std::atoi(optarg);
-                break;
-            }
-            case 'l': {
-                ASYNC_LOG_MODE = std::atoi(optarg);
-                break;
-            }
-            case 's': {
-                SQL_CONNECT_NUMS = std::atoi(optarg);
-                break;
-            }
-            case 't': {
-                THREAD_NUMS = std::atoi(optarg);
-                break;
-            }
-            case 'h': {
+    for (int i = 1; i < argc; ++i) {
+        if (argv[i][0] == '-') {
+            char option = argv[i][1];
+            if (option == 'h') {
                 std::cout << "Usage: " << argv[0] << " [options]\n"
                           << "Options:\n"
-                          << "  -p <port>                   Set the port number (default: 9090)\n"
-                          << "  -l <async_log_mode>         Set the log write mode (0: async, 1: sync)\n"
-                          << "  -s <sql_connect_nums>       Set the number of SQL connection (default: 8)\n"
-                          << "  -t <thread_mode>            Set the number of threads (default: 8)\n"
-                          << "  -h                          Show help\n";
-                exit(0);  // 显示帮助信息后退出程序
+                          << "  -p[:]<port>                Set the port number (default: 8080)\n"
+                          << "  -l[:]<async_log_mode>      Set the log write mode (0: async, 1: sync)\n"
+                          << "  -c[:]<sql_connect_nums>    Set the number of SQL connections (default: 8)\n"
+                          << "  -t[:]<thread_nums>         Set the number of threads (default: 8)\n"
+                          << "  -h                         Show help\n";
+                exit(0);
             }
-            default: {
-                std::cerr << "Unknown option: " << opt << ". Use -h for help.\n";
-                exit(1);  // 遇到未知选项时退出程序
+            
+            const char* value = nullptr;
+            if (argv[i][2] == ':') {
+                value = argv[i] + 3;
+            } else if (argv[i][2] != '\0') {
+                value = argv[i] + 2;
+            } else if (i + 1 < argc) {
+                value = argv[++i];
+            } else {
+                std::cerr << "Option -" << option << " requires a value.\n";
+                exit(1);
             }
+
+            switch (option) {
+                case 'p':
+                    PORT = std::atoi(value);
+                    break;
+                case 'l':
+                    ASYNC_LOG_MODE = std::atoi(value);
+                    break;
+                case 'c':
+                    SQL_CONNECT_NUMS = std::atoi(value);
+                    break;
+                case 't':
+                    THREAD_NUMS = std::atoi(value);
+                    break;
+                default:
+                    std::cerr << "Unknown option: -" << option << ". Use -h for help.\n";
+                    exit(1);
+            }
+        } else {
+            std::cerr << "Invalid argument: " << argv[i] << ". Use -h for help.\n";
+            exit(1);
         }
     }
 }
